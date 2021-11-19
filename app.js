b1f9fb5748413aa2a3a1eab19a177ef56507bb5c
Prakhar <prakhardabkara@gmail.com>
2021-11-19 21:32:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const nav_slide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        nav.classList.style.display="flex";
+    })
+
+}
+
+nav_slide();
+// Automatic slideshow
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
